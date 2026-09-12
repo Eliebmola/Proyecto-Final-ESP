f9fa7736e87bc5877025fd840acddf6ff753f89f
Emola123 <molaelieb94@gmail.com>
2026-09-11 20:37:06 -0500

    Modificaciones en usuarios

--- src/main/java/com/espfinal/projfinal/entities/Usuario.java
+++ src/main/java/com/espfinal/projfinal/entities/Usuario.java
@@ -18,7 +18,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(nullable = false, length = 100)
     private String nombre;
 
     @Column(nullable = false, unique = true, length = 100)
